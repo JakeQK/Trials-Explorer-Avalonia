9eecf898e993c349b92d47be968f077f5baa9abb
Jake Thornton <jt021101@gmail.com>
2025-03-08 19:00:53 +1000

    Avalonia project init

---
+++ Trials-Explorer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Trials_Explorer.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Trials-Explorer/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Trials_Explorer.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
